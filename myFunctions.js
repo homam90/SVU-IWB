b37576d374024b4a40a379c9e4c57a7014cd8bf7
homam90 <homamshm@gmail.com>
2024-06-10 12:56:15 +0300

    Add files via upload

---
+++ myFunctions.js
@@ -0,0 +1,160 @@
+﻿function checkarabic() {
+    const inpu = document.getElementById("clientname");
+    const textInput = document.getElementById("clientname").value.trim();
+    const arabicletters = /^[\u0600-\u06FF]+$/;
+
+
+    const words = textInput.split(/\s+/);
+
+    const ar = words.every(word => arabicletters.test(word));
+
+
+    if (textInput.length != 0 && ar == false) {
+        alert("يرجى ادخال الاسم باللغة العربية حصرا");
+        inpu.select();
+        inpu.focus();
+        return false;
+    }
+    return true;
+
+};
+
+function checkid() {
+
+
+    const inu = document.getElementById("nationalid");
+    const numberinput = document.getElementById("nationalid").value;
+    const pattern = /^(01|02|03|04|05|06|07|08|09|10|11|12|13|14)\d{9}$/;
+    const res = pattern.test(numberinput);
+    //const p = isNaN(numberinput);
+
+
+
+    if (res == false) {
+        alert("يرجى ادخال الرقم الوطني بشكل صحيح");
+        inu.select();
+        inu.focus();
+        return false;
+
+    }
+    return true;
+
+};
+
+function checkphone() {
+    const mobilein = document.getElementById("mobile");
+    const mobileinput = document.getElementById("mobile").value;
+    const patternmobile = /^(094|099|095|096|093|098)\d{7}$/;
+    const resul = patternmobile.test(mobileinput);
+    //const p = isNaN(numberinput);
+
+
+
+    if (resul == false && mobileinput.length != 0) {
+        alert("يرجى ادخال رقم الموبايل بشكل صحيح");
+        mobilein.select();
+        mobilein.focus();
+        return false;
+
+    }
+    return true;
+};
+
+function checkmail() {
+    const mailin = document.getElementById("mail");
+    const mailinput = document.getElementById("mail").value;
+    const patternmail = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    const resulmail = patternmail.test(mailinput);
+  
+
+
+
+    if (resulmail == false && mailinput.length != 0) {
+        alert("يرجى ادخال الايميل بشكل صحيح");
+        mailin.focus();
+        mailin.select();
+        return false;
+
+    }
+    return true;
+
+};
+function GC() {
+    const charsArray = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    let ca = '';
+    for (let i = 0; i < 6; i++) {
+        const randomChar = charsArray[Math.floor(Math.random() * charsArray.length)];
+        ca += randomChar;
+    }
+    document.getElementById('cinput').innerHTML = ca;
+
+};
+function codecheck()
+{
+    let codem = '';
+    let codec = '';
+    codem = document.getElementById('cinput').innerHTML;
+    codec = document.getElementById('cclient').value;
+    if (codem != codec) {
+        alert("يرجى التاكد من رمز التحقق");
+        return false;
+    }
+    return true;
+};
+
+function openNewWindow() {
+    if (checkid() && codecheck() && checkmail() && checkphone() && checkarabic() && checkchoice())
+    {
+        let ch= selectchoice();
+        
+        
+           
+            var newWindow = window.open("", "newWindow", "width=600,height=400");
+
+            newWindow.document.write("<html><head><title>Summary</title></head><body>");
+            newWindow.document.write("<h1>مواصفات العقار</h1>");
+            newWindow.document.write("<p>" + ch + "</p>");
+
+            newWindow.document.write("</body></html>");
+            newWindow.document.close();
+      
+      
+ }
+};
+
+function selectchoice()
+{
+    let ra = document.getElementsByName('estate'); 
+ 
+    let selectedValue;
+    for (var i = 0; i < ra.length; i++)
+    {
+        if (ra[i].checked)
+        {
+            selectedValue = ra[i].value;
+            break;
+        }
+    }
+    return selectedValue;
+
+};
+
+function checkchoice()
+{
+    let re = document.getElementsByName('estate');
+
+    var radioChecked = false;
+    for (var i = 0; i < re.length; i++) {
+        if (re[i].checked) {
+            radioChecked = true;
+            break;
+        }
+    }
+    if (!radioChecked)
+    {
+        alert('يرجى اختيار عقار');
+        return false;
+    }
+
+    return true;
+};
